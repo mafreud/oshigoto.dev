e2e691593119dab7f92b6fa75c277862b13a19da
Masahiro Aoki <rafe.kun@gmail.com>
2021-10-10 17:39:35 +0900

    Footerを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import Header from "../components/Header"
 import StackedLists from "../components/StackedLists"
+import Footer from "../components/Footer";
 
 export default function Index(){
     return (
+   <>
     <body className = 'bg-gray-100 h-screen'>
     <Header/>
          <div className = 'px-8 py-2'>
             <StackedLists/>
         </div>
      </body>
+             <Footer/>
+   </>
+
         );
 }
