40e1af8c74d172d84814f32574cb666ab70d8691
Masahiro Aoki <rafe.kun@gmail.com>
2021-10-15 18:20:20 +0900

    株式会社CARCH + 株式会社flixyを追加

--- data/jobList.ts
+++ data/jobList.ts
@@ -1,22 +1,42 @@
 export const jobList = [
-    {
-      id: 1,
-      title: 'Flutter + Firebase Developer',
-      type: 'フルタイム',
-      location: 'リモート/渋谷',
-      company: '株式会社Holox',
-      closeDate: '2022-01-01',
-      closeDateFull: 'January 1, 2022',
-      contact:'https://twitter.com/ma_freud'
-    },
-    {
-      id: 2,
-      title: 'Flutter + Firebase Developer',
-      type: '副業',
-      location: 'リモート/渋谷',
-      company: '株式会社Holox',
-      closeDate: '2022-01-01',
-      closeDateFull: 'January 1, 2022',
-      contact:'https://twitter.com/ma_freud'
-    },
-  ]
+  {
+    id: 0,
+    title: "Flutter Developer",
+    type: "フルタイム",
+    location: "リモート/浜松町",
+    company: "株式会社flixy",
+    closeDate: "2022-01-01",
+    closeDateFull: "January 1, 2022",
+    contact: "https://hrmos.co/pages/jmdc/jobs/0000221",
+  },
+  {
+    id: 0,
+    title: "Flutter + Firebase Developer",
+    type: "副業",
+    location: "リモート/上野",
+    company: "株式会社CARCH",
+    closeDate: "2022-01-01",
+    closeDateFull: "January 1, 2022",
+    contact: "https://twitter.com/rmoro000",
+  },
+  {
+    id: 0,
+    title: "Flutter + Firebase Developer",
+    type: "フルタイム",
+    location: "リモート/渋谷",
+    company: "株式会社Holox",
+    closeDate: "2022-01-01",
+    closeDateFull: "January 1, 2022",
+    contact: "https://twitter.com/ma_freud",
+  },
+  {
+    id: 0,
+    title: "Flutter + Firebase Developer",
+    type: "副業",
+    location: "リモート/渋谷",
+    company: "株式会社Holox",
+    closeDate: "2022-01-01",
+    closeDateFull: "January 1, 2022",
+    contact: "https://twitter.com/ma_freud",
+  },
+];
